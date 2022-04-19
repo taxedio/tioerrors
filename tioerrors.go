53874464309fa19a1305814664a0ad2035b5b5df
Peter Donald <peterdonald3@outlook.com>
2022-04-19 15:01:17 +0100

    TIOER-2 rebuilt go mod  init refactored package added test functions added LICENSE

--- tioerrors.go
+++ tioerrors.go
@@ -1,4 +1,4 @@
-package taxedio_errors
+package tioerrors
 
 import (
 	"encoding/json"
@@ -38,13 +38,99 @@ func (e restErr) Causes() []interface{} {
 	return e.ErrCauses
 }
 
-func NewRestError(message string, status int, err string, causes []interface{}) RestErr {
-	return restErr{
+func HttpStatusMaker(r restErr) *restErr {
+	switch r.Status() {
+	// 400s
+	case http.StatusBadRequest: // 400
+		r.ErrError = "bad_request"
+	case http.StatusPaymentRequired: // 402
+		r.ErrError = "payment_required"
+	case http.StatusForbidden: // 403
+		r.ErrError = "forbidden"
+	case http.StatusNotFound: // 404
+		r.ErrError = "not_found"
+	case http.StatusMethodNotAllowed: // 405
+		r.ErrError = "method_not_allowed"
+	case http.StatusNotAcceptable: // 406
+		r.ErrError = "method_not_allowed"
+	case http.StatusProxyAuthRequired: // 407
+		r.ErrError = "proxy_auth_required"
+	case http.StatusRequestTimeout: // 408
+		r.ErrError = "request_timeout"
+	case http.StatusConflict: // 409
+		r.ErrError = "conflict"
+	case http.StatusGone: // 410
+		r.ErrError = "gone"
+	case http.StatusLengthRequired: // 411
+		r.ErrError = "length_required"
+	case http.StatusPreconditionFailed: // 412
+		r.ErrError = "precondition_failed"
+	case http.StatusRequestEntityTooLarge: // 413
+		r.ErrError = "payload_too_large"
+	case http.StatusRequestURITooLong: // 414
+		r.ErrError = "uri_too_long"
+	case http.StatusUnsupportedMediaType: // 415
+		r.ErrError = "unsupported_media_type"
+	case http.StatusRequestedRangeNotSatisfiable: // 416
+		r.ErrError = "range_not_satisfiable"
+	case http.StatusExpectationFailed: // 417
+		r.ErrError = "expectation_failed"
+	case http.StatusTeapot: // 418
+		r.ErrError = "im_a_teapot"
+	case http.StatusMisdirectedRequest: // 421
+		r.ErrError = "misdirected_request"
+	case http.StatusUnprocessableEntity: // 422
+		r.ErrError = "unprocessable_entity"
+	case http.StatusLocked: // 423
+		r.ErrError = "locked"
+	case http.StatusFailedDependency: // 424
+		r.ErrError = "failed_dependency"
+	case http.StatusTooEarly: // 425
+		r.ErrError = "too_early"
+	case http.StatusUpgradeRequired: // 426
+		r.ErrError = "upgrade_required"
+	case http.StatusPreconditionRequired: // 428
+		r.ErrError = "precondition_required"
+	case http.StatusTooManyRequests: // 429
+		r.ErrError = "too_many_requests"
+	case http.StatusRequestHeaderFieldsTooLarge: // 431
+		r.ErrError = "request_header_fields_too_large"
+	case http.StatusUnavailableForLegalReasons: // 451
+		r.ErrError = "unavailable_for_legal_reasons"
+	// 500s
+	case http.StatusInternalServerError: // 500
+		r.ErrError = "internal_server_error"
+	case http.StatusNotImplemented: // 501
+		r.ErrError = "not_implemented"
+	case http.StatusBadGateway: // 502
+		r.ErrError = "bad_gateway"
+	case http.StatusServiceUnavailable: // 503
+		r.ErrError = "service_unavailable"
+	case http.StatusGatewayTimeout: // 504
+		r.ErrError = "gateway_timeout"
+	case http.StatusHTTPVersionNotSupported: // 505
+		r.ErrError = "http_version_not_supported"
+	case http.StatusVariantAlsoNegotiates: // 506
+		r.ErrError = "variant_also_negotiates"
+	case http.StatusInsufficientStorage: // 507
+		r.ErrError = "insufficent_storage"
+	case http.StatusLoopDetected: // 508
+		r.ErrError = "loop_detected"
+	case http.StatusNotExtended: // 510
+		r.ErrError = "not_extended"
+	case http.StatusNetworkAuthenticationRequired: // 511
+		r.ErrError = "network_authentication_required"
+	}
+	return &r
+}
+
+func NewRestError(message string, status int, causes []interface{}) restErr {
+	r := restErr{
 		ErrMessage: message,
 		ErrStatus:  status,
-		ErrError:   err,
 		ErrCauses:  causes,
 	}
+	return *HttpStatusMaker(r)
 }
 
 func NewRestErrorFromBytes(bytes []byte) (RestErr, error) {
@@ -53,40 +139,4 @@ func NewRestErrorFromBytes(bytes []byte) (RestErr, error) {
 		return nil, errors.New("invalid json")
 	}
 	return apiErr, nil
-}
-
-func NewBadRequestError(message string) RestErr {
-	return restErr{
-		ErrMessage: message,
-		ErrStatus:  http.StatusBadRequest,
-		ErrError:   "bad_request",
-	}
-}
-
-func NewNotFoundError(message string) RestErr {
-	return restErr{
-		ErrMessage: message,
-		ErrStatus:  http.StatusNotFound,
-		ErrError:   "not_found",
-	}
-}
-
-func NewUnauthorizedError(message string) RestErr {
-	return restErr{
-		ErrMessage: message,
-		ErrStatus:  http.StatusUnauthorized,
-		ErrError:   "unauthorized",
-	}
-}
-
-func NewInternalServerError(message string, err error) RestErr {
-	result := restErr{
-		ErrMessage: message,
-		ErrStatus:  http.StatusInternalServerError,
-		ErrError:   "internal_server_error",
-	}
-	if err != nil {
-		result.ErrCauses = append(result.ErrCauses, err.Error())
-	}
-	return result
 }
--- tioerrors_test.go
+++ tioerrors_test.go
@@ -1,33 +1,268 @@
-package taxedio_errors
+package tioerrors
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"reflect"
 	"testing"
-
-"github.com/stretchr/testify/assert"	
 )
 
-func TestNewInternalServerError(t *testing.T) {
-	err := NewInternalServerError("this is the message", errors.New("database error"))
-	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusInternalServerError, err.Status())
-	assert.EqualValues(t, "this is the message", err.Message())
-	assert.EqualValues(t, "message: this is the message - status: 500 - error: internal_server_error - causes: [database error]", err.Error())
-
-	assert.NotNil(t, err.Causes)
-	assert.EqualValues(t, 1, len(err.Causes()))
-	assert.EqualValues(t, "database error", err.Causes()[0])
+type codePair struct {
+	i int
+	s string
 }
 
-func TestNewBadRequestError(t *testing.T) {
-	//TODO: Test!
+var (
+	test1 = restErr{
+		ErrMessage: "testmessage",
+		ErrStatus:  http.StatusBadRequest,
+		ErrError:   "bad_request",
+	}
+	test1Bytes = []byte{123, 34, 109, 101, 115, 115, 97, 103, 101,
+		34, 58, 34, 116, 101, 115, 116, 109, 101, 115, 115, 97, 103,
+		101, 34, 44, 34, 115, 116, 97, 116, 117, 115, 34, 58, 52, 48,
+		48, 44, 34, 101, 114, 114, 111, 114, 34, 58, 34, 98, 97, 100,
+		95, 114, 101, 113, 117, 101, 115, 116, 34, 44, 34, 99, 97, 117,
+		115, 101, 115, 34, 58, 110, 117, 108, 108, 125}
+
+	test1BytesFail = []byte{123, 34, 109, 101, 115, 115, 97, 103, 101,
+		34, 58, 34, 116, 101, 115, 116, 109, 101, 115, 115, 97, 103,
+		101, 34, 44, 34, 115, 116, 97, 116, 117, 115, 34, 58, 52, 48,
+		48, 44, 34, 101, 114, 114, 111, 114, 34, 58, 34, 98, 97, 100,
+		95, 114, 101, 113, 117, 101}
+
+	codePairTests = []codePair{
+		{http.StatusBadRequest,
+			"bad_request"},
+		{http.StatusPaymentRequired,
+			"payment_required"},
+		{http.StatusForbidden, // 403 ,
+			"forbidden"},
+		{http.StatusNotFound, // 404,
+			"not_found"},
+		{http.StatusMethodNotAllowed, // 405,
+			"method_not_allowed"},
+		{http.StatusNotAcceptable, // 406,
+			"method_not_allowed"},
+		{http.StatusProxyAuthRequired, // 407,
+			"proxy_auth_required"},
+		{http.StatusRequestTimeout, // 408,
+			"request_timeout"},
+		{http.StatusConflict, // 409,
+			"conflict"},
+		{http.StatusGone, // 410,
+			"gone"},
+		{http.StatusLengthRequired, // 411,
+			"length_required"},
+		{http.StatusPreconditionFailed, // 412,
+			"precondition_failed"},
+		{http.StatusRequestEntityTooLarge, // 413,
+			"payload_too_large"},
+		{http.StatusRequestURITooLong, // 414,
+			"uri_too_long"},
+		{http.StatusUnsupportedMediaType, // 415,
+			"unsupported_media_type"},
+		{http.StatusRequestedRangeNotSatisfiable, // 416,
+			"range_not_satisfiable"},
+		{http.StatusExpectationFailed, // 417,
+			"expectation_failed"},
+		{http.StatusTeapot, // 418,
+			"im_a_teapot"},
+		{http.StatusMisdirectedRequest, // 421,
+			"misdirected_request"},
+		{http.StatusUnprocessableEntity, // 422,
+			"unprocessable_entity"},
+		{http.StatusLocked, // 423,
+			"locked"},
+		{http.StatusFailedDependency, // 424,
+			"failed_dependency"},
+		{http.StatusTooEarly, // 425,
+			"too_early"},
+		{http.StatusUpgradeRequired, // 426,
+			"upgrade_required"},
+		{http.StatusPreconditionRequired, // 428,
+			"precondition_required"},
+		{http.StatusTooManyRequests, // 429,
+			"too_many_requests"},
+		{http.StatusRequestHeaderFieldsTooLarge, // 431,
+			"request_header_fields_too_large"},
+		{http.StatusUnavailableForLegalReasons, // 451,
+			"unavailable_for_legal_reasons"},
+		{http.StatusInternalServerError, // 500,
+			"internal_server_error"},
+		{http.StatusNotImplemented, // 501,
+			"not_implemented"},
+		{http.StatusBadGateway, // 502,
+			"bad_gateway"},
+		{http.StatusServiceUnavailable, // 503,
+			"service_unavailable"},
+		{http.StatusGatewayTimeout, // 504,
+			"gateway_timeout"},
+		{http.StatusHTTPVersionNotSupported, // 505,
+			"http_version_not_supported"},
+		{http.StatusVariantAlsoNegotiates, // 506,
+			"variant_also_negotiates"},
+		{http.StatusInsufficientStorage, // 507,
+			"insufficent_storage"},
+		{http.StatusLoopDetected, // 508,
+			"loop_detected"},
+		{http.StatusNotExtended, // 510,
+			"not_extended"},
+		{http.StatusNetworkAuthenticationRequired, // 511,
+			"network_authentication_required"},
+	}
+)
+
+func Test_restErr_Error(t *testing.T) {
+	tests := []struct {
+		name string
+		e    restErr
+		want string
+	}{
+		{"test 1", test1, "message: testmessage - status: 400 - error: test1 - causes: []"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Error(); got != tt.want {
+				t.Errorf("restErr.Error() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
-func TestNewNotFoundError(t *testing.T) {
-	//TODO: Test!
+func Test_restErr_Message(t *testing.T) {
+	tests := []struct {
+		name string
+		e    restErr
+		want string
+	}{
+		{"test 1", test1, "testmessage"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fmt.Printf("%#v %T", tt.e.ErrCauses, tt.e.ErrCauses)
+			if got := tt.e.Message(); got != tt.want {
+				t.Errorf("restErr.Message() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
-func TestNewError(t *testing.T) {
-	//TODO: Test!
+func Test_restErr_Status(t *testing.T) {
+	tests := []struct {
+		name string
+		e    restErr
+		want int
+	}{
+		{"test 1", test1, http.StatusBadRequest},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Status(); got != tt.want {
+				t.Errorf("restErr.Status() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_restErr_Causes(t *testing.T) {
+	tests := []struct {
+		name string
+		e    restErr
+		want []interface{}
+	}{
+		{"test 1", test1, nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Causes(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("restErr.Causes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHttpStatusMaker(t *testing.T) {
+	type args struct {
+		r []codePair
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"test1", args{r: codePairTests}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, v := range tt.args.r {
+				inR := restErr{
+					ErrMessage: "test",
+					ErrStatus:  v.i,
+				}
+				wantR := restErr{
+					ErrMessage: "test",
+					ErrStatus:  v.i,
+					ErrError:   v.s,
+				}
+				if got := HttpStatusMaker(inR); !reflect.DeepEqual(got, &wantR) {
+					t.Errorf("HttpStatusMaker() = %v, want %v", got, wantR)
+				}
+			}
+
+		})
+	}
+}
+
+func TestNewRestError(t *testing.T) {
+	type args struct {
+		message string
+		status  int
+		causes  []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want restErr
+	}{
+		{"Test 1", args{message: "testmessage", status: http.StatusBadRequest, causes: nil}, test1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewRestError(tt.args.message, tt.args.status, tt.args.causes); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewRestError() = %v, want %v", got, tt.want)
+			} else {
+				by, _ := json.Marshal(got)
+				fmt.Println(by)
+			}
+		})
+	}
+}
+
+func TestNewRestErrorFromBytes(t *testing.T) {
+	type args struct {
+		bytes []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    RestErr
+		wantErr bool
+	}{
+		{"Test 1", args{bytes: test1Bytes}, test1, false},
+		{"Test 2", args{bytes: test1BytesFail}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewRestErrorFromBytes(tt.args.bytes)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewRestErrorFromBytes() error = %v, wantErr %v", (err != nil), tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewRestErrorFromBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
